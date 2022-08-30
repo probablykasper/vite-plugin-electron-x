8aef97e3445494257b4137252e138d5e5b11f187
Kasper <kasperkh.kh@gmail.com>
2022-08-30 11:17:06 +0200

    Switch back to vite

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
 import { spawn } from 'child_process'
-import { resolve, join } from 'path'
-import { default as esbuild, type BuildOptions as EsbuildOptions } from 'esbuild'
-import { BuildOptions as ViteBuildOptions, Plugin, ResolvedConfig } from 'vite'
+import path from 'path'
+import {
+	Plugin,
+	ResolvedConfig,
+	build as viteBuild,
+	BuildOptions as ViteBuildOptions,
+	ViteDevServer,
+} from 'vite'
 import fs from 'fs'
+import { builtinModules } from 'module'
 
-const electronDir = resolve(process.cwd(), 'node_modules', 'electron')
+const electronDir = path.resolve(process.cwd(), 'node_modules', 'electron')
 
 // imitates `electron/index.js`
 function getElectronPath() {
-	const pathFile = join(electronDir, 'path.txt')
-	console.log(pathFile)
+	const pathFile = path.join(electronDir, 'path.txt')
 
 	let executablePath: string | undefined
 	if (fs.existsSync(pathFile)) {
 		executablePath = fs.readFileSync(pathFile, 'utf-8')
 	}
 	if (process.env.ELECTRON_OVERRIDE_DIST_PATH) {
-		return join(process.env.ELECTRON_OVERRIDE_DIST_PATH, executablePath || 'electron')
+		return path.join(process.env.ELECTRON_OVERRIDE_DIST_PATH, executablePath || 'electron')
 	}
 	if (executablePath) {
-		return join(electronDir, 'dist', executablePath)
+		return path.join(electronDir, 'dist', executablePath)
 	}
 	console.error('Could not get Electron path')
 	process.exit(1)
@@ -46,35 +51,54 @@ async function spawnElectron(args: string[], env: NodeJS.ProcessEnv) {
 	process.on('SIGTERM', terminationHandler)
 }
 
-async function resolveOutDir(options: BuildOptions, viteConfig: ResolvedConfig) {
-	return options.outDir || resolve(viteConfig.build.outDir, 'electron')
-}
-function resolveSourcemap(sourcemap: ViteBuildOptions['sourcemap']): EsbuildOptions['sourcemap'] {
-	if (sourcemap === true) return 'linked'
-	else if (sourcemap === 'inline') return 'linked'
-	else if (sourcemap === 'hidden') return 'external'
-	else return false
-}
-
-async function build(options: BuildOptions, viteConfig: ResolvedConfig): Promise<string> {
-	const outDir = await resolveOutDir(options, viteConfig)
-
-	await esbuild.build({
-		entryPoints: [options.entry],
-		outfile: resolve(outDir, 'main.js'),
-		platform: 'node',
-		bundle: true,
-		format: 'cjs',
-		target: 'node16',
-		minify: !!viteConfig.build.minify,
-		// define,
-		sourcemap: resolveSourcemap(viteConfig.build.sourcemap),
-		// inject
-		// plugins
-		external: ['electron', ...(options.external || [])],
+async function build(options: Required<BuildOptions>, viteConfig: ResolvedConfig): Promise<string> {
+	const output = await viteBuild({
+		configFile: false,
+		publicDir: false,
+		mode: 'production',
+		build: {
+			target: options.target,
+			minify: viteConfig.build.minify,
+			emptyOutDir: false,
+			sourcemap: viteConfig.build.sourcemap,
+			outDir: options.outDir,
+			lib: {
+				entry: options.entry,
+				formats: ['cjs'],
+			},
+			rollupOptions: {
+				external: [
+					'electron',
+					...builtinModules.flatMap((m) => [m, `node:${m}`]),
+					...options.external.map((p) => path.resolve(p)),
+				],
+				output: {
+					entryFileNames: '[name].js',
+				},
+			},
+		},
 	})
 
-	return resolve(outDir, 'main.js')
+	if ('addListener' in output) {
+		console.error('Unexpected RollupWatcher')
+		process.exit(1)
+	}
+	const outputs = [output].flat()
+	if (outputs.length > 1) {
+		console.error('Too many build outputs')
+		process.exit(1)
+	}
+	const entries = []
+	for (const item of outputs[0].output) {
+		if (item.type === 'chunk' && item.isEntry) {
+			entries.push(item)
+		}
+	}
+	if (entries.length !== 1) {
+		console.error('Expected 1 output entry, got', entries.length)
+		process.exit(1)
+	}
+	return path.resolve(options.outDir, entries[0].fileName)
 }
 
 function rmDirIfExists(path: string): Promise<void> {
@@ -87,18 +111,82 @@ function rmDirIfExists(path: string): Promise<void> {
 		})
 	})
 }
-async function handleOutDirCleaning(options: Options, viteConfig: ResolvedConfig) {
-	if (!viteConfig.build.emptyOutDir) {
-		return
+async function handleOutDirCleaning(options: ResolvedOptions, viteConfig: ResolvedConfig) {
+	if (!viteConfig.build.emptyOutDir) return
+	if (options.main !== false) await rmDirIfExists(options.main.outDir)
+	if (options.preload) await rmDirIfExists(options.preload.outDir)
+}
+
+function resolveBuildOptions(
+	outDirName: 'electron' | 'preload',
+	options: BuildOptions | false,
+	viteConfig: ResolvedConfig
+): Required<BuildOptions> | false {
+	if (options === false) return false
+	return {
+		outDir: options.outDir || path.resolve(viteConfig.build.outDir, outDirName),
+		entry: options.entry,
+		external: options.external || [],
+		sourcemap: options.sourcemap || viteConfig.build.sourcemap,
+		target: options.target || 'node16',
 	}
-	if (options.main !== false) {
-		const outDir = await resolveOutDir(options.main, viteConfig)
-		await rmDirIfExists(outDir)
+}
+function resolveDevOptions(
+	dev: DevOptions | false,
+	main: BuildOptions | false
+): Required<DevOptions> | false {
+	if (dev === false) return false
+
+	console.log('redev', !!dev.entry, main)
+
+	let entry
+	if (dev.entry) {
+		entry = dev.entry
+	} else if (main && main.outDir) {
+		const basename = path.basename(main.entry)
+		entry = path.resolve(main.outDir, basename)
+		console.log('--------', entry)
+	} else {
+		console.error(
+			'No entry point found for Electron dev server. You must specify `main.entry` or `dev.entry`'
+		)
+		process.exit(1)
 	}
-	if (options.preload) {
-		const outDir = await resolveOutDir(options.preload, viteConfig)
-		await rmDirIfExists(outDir)
+
+	return {
+		env: dev.env || {},
+		entry,
 	}
+}
+function resolveOptions(options: Options, viteConfig: ResolvedConfig): ResolvedOptions {
+	const main = resolveBuildOptions('electron', options.main, viteConfig)
+	// const mainOurEntry = main.
+	return {
+		main,
+		preload: resolveBuildOptions('preload', options.preload || false, viteConfig),
+		dev: resolveDevOptions(options.dev || {}, main),
+	}
+}
+
+function dev(server: ViteDevServer, options: Required<DevOptions>) {
+	server.httpServer?.once('listening', async () => {
+		const address = server.httpServer?.address() || null
+		if (address === null || typeof address === 'string') {
+			console.error('Unexpected dev server address', address)
+			process.exit(1)
+		}
+		const protocol = server.config.server.https ? 'https' : 'http'
+		const hostname = localhosts.includes(address.address) ? 'localhost' : address.address
+		const defaultEnv = {
+			VITE_DEV_SERVER_URL: `${protocol}://${hostname}:${address.port}`,
+			VITE_DEV_SERVER_HOSTNAME: hostname,
+			VITE_DEV_SERVER_PORT: address.port.toString(),
+		}
+		const env = { ...defaultEnv, ...options.env }
+
+		console.log('\nStarting Electron...')
+		spawnElectron([options.entry], env)
+	})
 }
 
 const localhosts = ['localhost', '127.0.0.1', '::1', '0000:0000:0000:0000:0000:0000:0000:0001']
@@ -113,12 +201,13 @@ export function electron(options: Options): Plugin[] {
 			viteConfig = config
 		},
 		async closeBundle() {
-			await handleOutDirCleaning(options, viteConfig)
-			if (options.main !== false) {
-				await build(options.main, viteConfig)
+			const resolvedOptions = resolveOptions(options, viteConfig)
+			await handleOutDirCleaning(resolvedOptions, viteConfig)
+			if (resolvedOptions.main !== false) {
+				await build(resolvedOptions.main, viteConfig)
 			}
-			if (options.preload) {
-				await build(options.preload, viteConfig)
+			if (resolvedOptions.preload) {
+				await build(resolvedOptions.preload, viteConfig)
 			}
 		},
 	}
@@ -131,67 +220,43 @@ export function electron(options: Options): Plugin[] {
 			viteConfig = config
 		},
 		async configureServer(server) {
-			await handleOutDirCleaning(options, viteConfig)
-			let mainOutPath: string | undefined
-			if (options.main !== false) {
-				mainOutPath = await build(options.main, viteConfig)
-			}
-			if (options.preload) {
-				await build(options.preload, viteConfig)
-			}
-
-			if (options.dev === false) {
-				return
-			}
-			const devOptions = options.dev || {}
-			const devEntry = devOptions.entry || mainOutPath
-			if (!devEntry) {
-				console.error(
-					'No entry point found for Electron dev server. You must specify `main.entry` or `dev.entry`'
-				)
-				process.exit(1)
-			}
-
-			server.httpServer?.once('listening', async () => {
-				const address = server.httpServer?.address() || null
-				if (address === null || typeof address === 'string') {
-					console.error('Unexpected dev server address', address)
-					process.exit(1)
+			const resolvedOptions = resolveOptions(options, viteConfig)
+			await handleOutDirCleaning(resolvedOptions, viteConfig)
+			if (resolvedOptions.main !== false) {
+				const outputEntry = await build(resolvedOptions.main, viteConfig)
+				if (resolvedOptions.dev !== false) {
+					resolvedOptions.dev.entry = outputEntry
 				}
-				const hostname = localhosts.includes(address.address) ? 'localhost' : address.address
-				const protocol = server.config.server.https ? 'https' : 'http'
-				const url = `${protocol}://${hostname}:${address.port}`
+			}
+			if (resolvedOptions.preload) {
+				await build(resolvedOptions.preload, viteConfig)
+			}
 
-				const env = devOptions.env || {}
-				if (!env.VITE_DEV_SERVER_URL) env.VITE_DEV_SERVER_URL = url
-				if (!env.VITE_DEV_SERVER_HOSTNAME) env.VITE_DEV_SERVER_HOSTNAME = hostname
-				if (!env.VITE_DEV_SERVER_PORT) env.VITE_DEV_SERVER_PORT = address.port.toString()
-
-				console.log('\nStarting Electron...')
-				spawnElectron([devEntry], env)
-			})
+			if (resolvedOptions.dev !== false) {
+				dev(server, resolvedOptions.dev)
+			}
 		},
 	}
 
 	return [buildPlugin, servePlugin]
 }
 
-export type BuildOptions = {
-	/** Your main or preload electron entrypoint */
-	entry: string
-	outDir?: string
-	/** What to target, like `node16` or `node14.17.0` */
-	target?: EsbuildOptions['target']
-	/** Override Vite's sourcemap option */
-	sourcemap?: EsbuildOptions['sourcemap']
-	/** Specify dependencies that shouldn't be bundled. Electron is always externalized. */
-	external?: EsbuildOptions['external']
-}
 export type DevOptions = {
 	/** @default {} */
 	env?: NodeJS.ProcessEnv
 	/** The electron entrypoint. You don't need to set this if you already have `main.entry`. */
 	entry?: string
+}
+export type BuildOptions = {
+	/** Your main or preload electron entrypoint */
+	entry: string
+	outDir?: string
+	/** What to target, like `node16` or `node14.17.0` */
+	target?: ViteBuildOptions['target']
+	/** Override Vite's sourcemap option */
+	sourcemap?: ViteBuildOptions['sourcemap']
+	/** Specify dependencies that shouldn't be bundled. Electron is always externalized. */
+	external?: string[]
 }
 export type Options = {
 	/** Setting this to `false` disables the dev server. */
@@ -201,4 +266,9 @@ export type Options = {
 	main: BuildOptions | false
 	/** @default false */
 	preload?: BuildOptions | false
+}
+type ResolvedOptions = {
+	dev: Required<DevOptions> | false
+	main: Required<BuildOptions> | false
+	preload: Required<BuildOptions> | false
 }
